4ef2a13a92492f3e268d2b879921eead9cd5094a
abasheryassin <abasheryassin@gmail.com>
2022-12-05 20:47:50 -0600

    Submit button and save respone

--- assets/script.js
+++ assets/script.js
@@ -1,28 +1,16 @@
-// const encodedParams = new URLSearchParams();
-// encodedParams.append("privateKey", "e0be486578d748d3413bbde846a19265cc745c2a");
-// encodedParams.append("nameStartsWith", "Spider-Man");
-// encodedParams.append("publicKey", "daa60ec964f3d078d4b5113c45d2896d");
-
-// const options = {
-// 	method: 'POST',
-// 	headers: {
-// 		'content-type': 'application/x-www-form-urlencoded',
-// 		'X-RapidAPI-Key': '95ca73bdfamshb3559b4cafeb6c0p1be87ajsnd385a82867f4',
-// 		'X-RapidAPI-Host': 'Marvelstefan-skliarovV1.p.rapidapi.com'
-// 	},
-// 	body: encodedParams
-// };
-
-// fetch('https://marvelstefan-skliarovv1.p.rapidapi.com/getCharacters', options)
-// 	.then(response => response.json())
-// 	.then(response => console.log(response))
-// 	.catch(err => console.error(err));
-	
-
 var newButton = document.createElement("button");
+var characterSubmit = document.getElementById('submit');
+var characterEnter = document.getElementById('userinput');
+
+characterSubmit.addEventListener('click', function() {
+	var enteredCharacter = characterEnter.value;
+	console.log(enteredCharacter);
+});
+
 newButton.textContent = "Hey!"
 newButton.onclick = function() {
-	fetch("https://gateway.marvel.com:443/v1/public/characters?apikey=daa60ec964f3d078d4b5113c45d2896d")
+	fetch("https://gateway.marvel.com/v1/public/characters?apikey=daa60ec964f3d078d4b5113c45d2896d")
+	//http://gateway.marvel.com/v1/public/comics?apikey=yourAPIKEY
 		.then(function(response) {
 			return response.json();
 		})
