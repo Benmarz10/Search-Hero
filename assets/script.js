d3f265ab6f5b77ce6e40ff0db089c168350aeb0d
NicholasHM05 <nhmamberger@gmail.com>
2022-12-07 16:29:03 -0600

    another push

--- assets/script.js
+++ assets/script.js
@@ -16,15 +16,15 @@ submitButton.on('click', function () {
 });
 
 // Get marvel character from user input
-function getMarvelCharacter(userInput) {
-	fetch("https://gateway.marvel.com:443/v1/public/characters?name=" + userInput + "&apikey=daa60ec964f3d078d4b5113c45d2896d")
-		.then(function (response) {
-			return response.json();
-		})
-		.then(function (data) {
-			console.log(data);
-		})
-}
+// function getMarvelCharacter(userInput) {
+// 	fetch("https://gateway.marvel.com:443/v1/public/characters?name=" + userInput + "&apikey=daa60ec964f3d078d4b5113c45d2896d")
+// 		.then(function (response) {
+// 			return response.json();
+// 		})
+// 		.then(function (data) {
+// 			console.log(data);
+// 		})
+// }
 
 
 // Get API with more info on WIKI or something
