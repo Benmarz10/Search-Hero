82656666c588d59f078fd461079221b545c5f5be
JoshuaHamann <joshwashku@yahoo.com>
2022-12-07 20:31:26 -0600

    added fetch to wikiAPI

--- assets/script.js
+++ assets/script.js
@@ -8,12 +8,12 @@ var submitButton = $('#search-modal');
 
 var savedCharactersList = {};
 
-submitButton.on('click', function () {
-	var userInput = userCharacter.val();
-	getMarvelCharacter(userInput);
-	console.log(userInput);
+// submitButton.on('click', function () {
+//   var userInput = userCharacter.val();
+//   getMarvelCharacter(userInput);
+//   console.log(userInput);
 
-});
+// });
 
 // Get marvel character from user input
 // function getMarvelCharacter(userInput) {
@@ -32,33 +32,56 @@ submitButton.on('click', function () {
 // Get and fetch marvel character from API and whatever attributes
 // Get data and append it to container
 //new var characterInfo created in here
-function getMarvelCharacter(userInput) {
-  fetch ("https://gateway.marvel.com:443/v1/public/characters?name=" + userInput + "&apikey=daa60ec964f3d078d4b5113c45d2896d")
-	.then(response => response.json())
-  .then(characterInfo => {
-console.log(characterInfo)
+submitButton.on('click', function () {
+  var userInput = userCharacter.val();
+//   getMarvelCharacter(userInput);
+  console.log(userInput);
 
-document.querySelector('#characterName').innerHTML = characterInfo.data.results[0].name;
-  })
- 
-              
+  fetch("https://gateway.marvel.com:443/v1/public/characters?name=" + userInput + "&apikey=daa60ec964f3d078d4b5113c45d2896d")
+    .then(response => response.json())
+    .then(characterInfo => {
+      console.log(characterInfo)
 
+      document.querySelector('#characterName').innerHTML = characterInfo.data.results[0].name;
+      document.querySelector('#description').innerHTML = characterInfo.data.results[0].description;
+      document.querySelector('#icon').innerHTML = characterInfo.data.results[0].thumbnail;
+
+    });
+})
+
+
+function getMoreInfoAPI(characterName){
+    fetch("https://en.wikipedia.org/w/api.php?action=query&origin=*&format=json"+
+    "&prop=info|extracts&inprop=url&generator=search&gsrnamespace=0&gsrlimit=5&gsrsearch="+characterName)
+    .then(function(response){
+        console.log(response);
+        return response.json();
+    })
+    .then(function(data){
+        console.log(data);
+        for (var i in data.query.pages) {
+            console.log(data.query.pages[i].title);
+        }
+    });
 }
+
+getMoreInfoAPI('Thor');
+
 
 function displayMarvelCharacter(characterInfo){
     
 }
 
 function handleSearchBtn(event) {
-	// Set local storage with click
+  // Set local storage with click
 }
 
 function handleMoreInfoBtn(event) {
-	// Fetch more infor when clicked
+  // Fetch more infor when clicked
 }
 
 function handleClearBtn(event) {
-	// Clear local storage and list if needed
+  // Clear local storage and list if needed
 }
 
 //	//search.addEventListener('click', handleSearchBtn);
@@ -67,60 +90,60 @@ function handleClearBtn(event) {
 
 //Testing Modal functions
 document.addEventListener('DOMContentLoaded', () => {
-	// Functions to open and close a modal
-	function openModal($el) {
-		$el.classList.add('is-active');
-	}
+  // Functions to open and close a modal
+  function openModal($el) {
+    $el.classList.add('is-active');
+  }
 
-	function closeModal($el) {
-		$el.classList.remove('is-active');
-	}
+  function closeModal($el) {
+    $el.classList.remove('is-active');
+  }
 
-	function closeAllModals() {
-		(document.querySelectorAll('.modal') || []).forEach(($modal) => {
-			closeModal($modal);
-		});
-	}
+  function closeAllModals() {
+    (document.querySelectorAll('.modal') || []).forEach(($modal) => {
+      closeModal($modal);
+    });
+  }
 
-	// Add a click event on buttons to open a specific modal
-	(document.querySelectorAll('.js-modal-trigger') || []).forEach(($trigger) => {
-		const modal = $trigger.dataset.target;
-		const $target = document.getElementById(modal);
+  // Add a click event on buttons to open a specific modal
+  (document.querySelectorAll('.js-modal-trigger') || []).forEach(($trigger) => {
+    const modal = $trigger.dataset.target;
+    const $target = document.getElementById(modal);
 
-		$trigger.addEventListener('click', () => {
-			openModal($target);
-		});
-	});
+    $trigger.addEventListener('click', () => {
+      openModal($target);
+    });
+  });
 
-	// Add a click event on various child elements to close the parent modal
-	(document.querySelectorAll('.modal-background, .modal-close, .modal-card-head .delete, .modal-card-foot .button') || []).forEach(($close) => {
-		const $target = $close.closest('.modal');
+  // Add a click event on various child elements to close the parent modal
+  (document.querySelectorAll('.modal-background, .modal-close, .modal-card-head .delete, .modal-card-foot .button') || []).forEach(($close) => {
+    const $target = $close.closest('.modal');
 
-		$close.addEventListener('click', () => {
-			closeModal($target);
-		});
-	});
+    $close.addEventListener('click', () => {
+      closeModal($target);
+    });
+  });
 
-	// Add a keyboard event to close all modals
-	document.addEventListener('keydown', (event) => {
-		const e = event || window.event;
+  // Add a keyboard event to close all modals
+  document.addEventListener('keydown', (event) => {
+    const e = event || window.event;
 
-		if (e.keyCode === 27) { // Escape key
-			closeAllModals();
-		}
-	});
+    if (e.keyCode === 27) { // Escape key
+      closeAllModals();
+    }
+  });
 });
 
-$(document).ready(function() {
-	$('.filter-menu').on('click', '#dropdown', event => {
-	  event.preventDefault();
-	  console.log('dropdown menu option clicked!');
-  
-	  var value = $(this).val(); // Here to get value
-	  if (value === 1){
-		const bookmarks = STORE.list.filter(bookmark => bookmark.rating >= 1);
-		return bookmarks;
-	  }
-	  renderStore();
-	});
+$(document).ready(function () {
+  $('.filter-menu').on('click', '#dropdown', event => {
+    event.preventDefault();
+    console.log('dropdown menu option clicked!');
+
+    var value = $(this).val(); // Here to get value
+    if (value === 1) {
+      const bookmarks = STORE.list.filter(bookmark => bookmark.rating >= 1);
+      return bookmarks;
+    }
+    renderStore();
   });
+});
